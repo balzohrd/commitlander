0890b50acbbbb26e0473993bbbc2b5f28c0ef50b
Austin Turnage <austin@datarats.com>
2015-03-25 15:47:58 -0400

    structure change and added quotes

---
+++ quotes/marvin.js
@@ -0,0 +1,6 @@
+module.exports = [
+  "Life! Don't talk to me about life",
+  "Life, loathe it or ignore it, you can't like it",
+  "Life's bad enough as it is without wanting to invent any more of it",
+  "Funny, how just when you think life can't possibly get any worse it suddenly does"
+]

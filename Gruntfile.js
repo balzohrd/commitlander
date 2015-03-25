7ea35014969fdf0e30c86b436c61f79f246cfd18
Austin Turnage <austin@datarats.com>
2015-03-25 13:12:57 -0400

    closes #1

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+/*!
+ * Commitlander
+ *
+ * Copyright(c) 2015 Austin Turnage
+ * MIT Licensed
+ */
+
+'use strict';
+module.exports = function (grunt) {
+  // Show elapsed time at the end
+  require('time-grunt')(grunt);
+  // Load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      gruntfile: {
+        src: ['Gruntfile.js']
+      },
+      js: {
+        src: ['bin/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      js: {
+        files: '<%= jshint.js.src %>',
+        tasks: ['jshint:js']
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['jshint']);
+};
